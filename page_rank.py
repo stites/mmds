ce35f41e1e86995d1b6e19bb35bd5b7be5f1ed12
Sam Stites <sam@stites.io>
2014-10-07 02:21:12 -0700

    polish

--- page_rank.py
+++ page_rank.py
@@ -6,14 +6,14 @@
 import numpy as np
 
 
-# In[280]:
+# In[421]:
 
 # Q1
 
 b = 0.7
 n = 3
 N = np.ones((n,1)) / 3;
-r = np.ones((n,1)) / 3;
+r = np.ones((n,1));
 
 M = np.array([
         [  0,   0,   0],
@@ -25,7 +25,28 @@ r = np.dot(b*M, r) + ((1-b)/N)
 print r
 
 
-# In[275]:
+# In[484]:
+
+
+r = np.dot(b*M, r) + ((1-b)/N)
+
+print r
+
+
+# In[486]:
+
+r = r / 3
+a = r[0][0]
+b = r[1][0]
+c = r[2][0]
+
+print 'a + b = ', a + b
+print 'a + b = ', a + b
+print 'b + c = ', b + c
+print 'a + c = ', a + c
+
+
+# In[486]:
 
 
 
