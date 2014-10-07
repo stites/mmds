fix - page rank formula

page_rank.py
@@ -6,22 +6,26 @@
 import numpy as np
 
 
-# In[171]:
+# In[280]:
 
 # Q1
 
 b = 0.7
 n = 3
-N = np.ones((n,n)) / 3;
+N = np.ones((n,1)) / 3;
+r = np.ones((n,1)) / 3;
 
 M = np.array([
-        [0, 0.5, 0.5],
-        [0,   0,   1],
-        [0,   0,   1]], dtype=np.float)
-A = np.add(b*M, (1-b)*N)
+        [  0,   0,   0],
+        [0.5,   0,   0],
+        [0.5,   1,   1]], dtype=np.float)
+
+r = np.dot(b*M, r) + ((1-b)/N)
+
+print r
 
 
-# In[151]:
+# In[275]:
 
 
 