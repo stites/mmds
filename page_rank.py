a6b784280a05ad7b0c7cbc2014281b4ca1776a58
Sam Stites <sam@stites.io>
2014-10-07 01:45:40 -0700

    fix - initial matirix

--- page_rank.py
+++ page_rank.py
@@ -6,23 +6,22 @@
 import numpy as np
 
 
-# In[33]:
+# In[171]:
 
 # Q1
 
 b = 0.7
 n = 3
-N = np.empty((n,n))
-N.fill(1/n)
+N = np.ones((n,n)) / 3;
+
 M = np.array([
         [0, 0.5, 0.5],
         [0,   0,   1],
         [0,   0,   1]], dtype=np.float)
-A = b*M + (1-b)*N
-print A
+A = np.add(b*M, (1-b)*N)
 
 
-# In[18]:
+# In[151]:
 
 
 
