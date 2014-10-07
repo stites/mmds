bbceb426459f949263c1207078338c980ab29fcd
Sam Stites <sam@stites.io>
2014-10-07 02:27:59 -0700

    define - function

--- page_rank.py
+++ page_rank.py
@@ -1,41 +1,34 @@
 
 # coding: utf-8
 
-# In[27]:
+# In[488]:
 
 import numpy as np
 
 
-# In[421]:
+# In[493]:
+
+def get_pagerank (b, n, M, iters, scale):
+    N = np.ones((n,1)) / n;
+    r = np.ones((n,1));
+    for x in xrange(iters):
+        r = np.dot(b*M, r) + ((1-b)/N)
+    r = r / scale
+    return r
+    
+
+
+# In[494]:
 
 # Q1
-
-b = 0.7
-n = 3
-N = np.ones((n,1)) / 3;
-r = np.ones((n,1));
-
-M = np.array([
-        [  0,   0,   0],
-        [0.5,   0,   0],
-        [0.5,   1,   1]], dtype=np.float)
-
-r = np.dot(b*M, r) + ((1-b)/N)
-
-print r
-
-
-# In[484]:
-
-
-r = np.dot(b*M, r) + ((1-b)/N)
-
-print r
-
-
-# In[486]:
-
-r = r / 3
+r = get_pagerank(0.7,
+                 3,
+                 np.array([
+                    [  0,   0,   0],
+                    [0.5,   0,   0],
+                    [0.5,   1,   1]], dtype=np.float),
+                 1000,
+                 3)
 a = r[0][0]
 b = r[1][0]
 c = r[2][0]
@@ -46,7 +39,17 @@ print 'b + c = ', b + c
 print 'a + c = ', a + c
 
 
+# In[484]:
+
+
+
+
 # In[486]:
+
+
+
+
+# In[491]:
 
 
 
